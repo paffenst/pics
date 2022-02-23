980e56a9b5dbab5845618860bc09657c93551efd
paffen <pavel.stoyanov0@hm.edu>
2022-02-23 19:45:06 +0100

    adding a css part of list

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,9 +1,11 @@
+import './ImageList.css';
 import React from 'react';
+ 
 
 const ImageList = props => {
     const images = props.images.map(({ discription, id, urls }) => {
         return <img alt={discription} key={id} src={urls.regular} />
     });
-    return <div>{images}</div>
+    return <div className="image-list">{images}</div>
 };
 export default ImageList;
